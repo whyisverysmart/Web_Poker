16f1d86a1afdd2482db4c089cc65757e1ec031e1
whyisverysmart <17717502004@163.com>
2024-01-16 16:05:16 +0800

    modified GameView

--- services/frontend/src/views/GameView.vue
+++ services/frontend/src/views/GameView.vue
@@ -1,14 +1,124 @@
+<style>
+.el-row {
+  margin-bottom: 20px;
+  height: 160px;
+}
+
+.el-col {
+  /*设置圆角*/
+  border-radius: 4px;
+  border: solid;
+  display: flex;
+}
+
+.my-card {
+    flex: 1;
+    border: solid;
+    margin: 5px;
+    font-size: large;
+    color: brown;
+}
+.public-card {
+    flex: 1;
+    font-size: large;
+    color: blue;
+    margin: 10px;
+    border: solid;
+}
+
+.el-col .desk-up {
+    display: flex;
+}
+
+.el-col .desk-down {
+    display: flex;
+}
+
+.down-component {
+    flex: 1;
+    font-size: large;
+    color: blue;
+    margin: 10px;
+    border: solid;
+}
+</style>
+
 <template>
-    <div class="home">
-        <p>This is the game page</p>
-    </div>
+    <br />
+    <el-row>
+        <el-col :span="6">
+            <div style="flex: 1;">Player 4 and information</div>
+            <div style="flex: 1;">
+                <p>Card 1</p>
+                <p>Card 2</p>
+            </div>
+        </el-col>
+        <el-col :span="6"><p>5</p></el-col>
+        <el-col :span="6"><p>6</p></el-col>
+        <el-col :span="6"><p>7</p></el-col>
+    </el-row>
+    <el-row>
+        <el-col :span="6"><p>3</p></el-col>
+        <el-col :span="12" class="desk-up">
+            <div class="public-card">Card 1</div>
+            <div class="public-card">Card 2</div>
+            <div class="public-card">Card 3</div>
+            <div class="public-card">Card 4</div>
+            <div class="public-card">Card 5</div>
+        </el-col>
+        <el-col :span="6"><p>8</p></el-col>
+    </el-row>
+    <el-row>
+        <el-col :span="6"><p>2</p></el-col>
+        <el-col :span="12" class="desk-down">
+            <div class="down-component">BB: 200?</div>
+            <div class="down-component">Port: 000?</div>
+            <div class="down-component">Time: 120s?</div>
+        </el-col>
+        <el-col :span="6"><p>9</p></el-col>
+    </el-row>
+    <el-row>
+        <el-col :span="6">1</el-col>
+
+        <!--Mine Part-->
+        <el-col :span="12">
+            <!--Mine Info-->
+            <div style="flex: 1;">Mine</div>
+            <!--Mine Cards-->
+            <div style="flex: 2; margin: 5px;">
+                <div style="display: flex; height: 100%;">
+                    <div class="my-card">Card 1</div>
+                    <div class="my-card">Card 2</div>
+                </div>
+            </div>
+            <!--Mine commit-->
+            <div style="flex: 2; margin: 20px;">
+                <div style="display: flex;">
+                    <el-input-number v-model="amount" size="large" :step="200" style="flex: 3;"/>
+                    <el-button type="success" size="large" style="flex: 1;">Raise</el-button>
+                </div>
+                <p>Your Tokens: 000</p>
+                <div style="display: flex;">
+                    <el-button type="primary" size="large" style="flex: 1;">Check</el-button>
+                    <el-button type="success" size="large" style="flex: 1;">Call</el-button>
+                    <el-button type="danger" size="large" style="flex: 1;">Fold</el-button>
+                </div>
+            </div>
+        </el-col><!-- /Mine -->
+
+        <el-col :span="6">10</el-col>
+    </el-row>
 </template>
 
 <script>
-
 export default {
     name: 'GameView',
-    components: {}
+    components: {},
+    data() {
+        return {
+            amount: 0,
+        }
+    }
 }
 </script>
   
